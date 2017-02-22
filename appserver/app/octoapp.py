ec991b683ee9453a0a49f3b32eb8b59699db09fc
Ash Wilson <ash.d.wilson@gmail.com>
2017-02-22 11:21:43 -0800

    chg: usr: Added job-list page in app server

--- appserver/app/octoapp.py
+++ appserver/app/octoapp.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask import render_template
+from octoutils.utility import Utility as util
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World, this is the OCTOBOX app!'
+def home_page():
+    return render_template('mainpage.html')
+
+@app.route('/job-list')
+def reports_page():
+    return render_template('job-list.html', tasks=util.get_celery_tasks)
---
+++ appserver/app/octoutils/utility.py
@@ -0,0 +1,30 @@
+import datetime
+import os
+import requests
+from urllib import urljoin
+
+class Utility(object):
+    @classmethod
+    def get_celery_tasks(cls):
+        retval = []
+        celery_url = urljoin(os.getenv("FLOWER_HOST"), "api/tasks")
+        result = requests.get(celery_url).json()
+        for task in result.items():
+            task_prefmt = {"id": task[0],
+                           "name": task[1]["name"],
+                           "args": str(task[1]["args"]),
+                           "kwargs": str(task[1]["kwargs"]),
+                           "started": Utility.u_to_8601(task[1]["started"]),
+                           "tstamp": Utility.u_to_8601(task[1]["timestamp"]),
+                           "state": task[1]["state"],
+                           "exception": str(task[1]["exception"])}
+            retval.append(task_prefmt)
+        return retval
+
+    @classmethod
+    def u_to_8601(cls, unixtime):
+        try:
+            ret = datetime.datetime.fromtimestamp(float(unixtime)).isoformat()
+        except TypeError:
+            ret = "N/A"
+        return ret
